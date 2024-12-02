0e4d1ff13876bc4e6d62731c3cf908edada2fdb9
OrestisL <liaskos.orestis@gmail.com>
2024-12-02 15:05:09 +0200

    Asteroid prefabs

--- Assets/Scripts/Asteroids/Asteroid.cs
+++ Assets/Scripts/Asteroids/Asteroid.cs
@@ -3,20 +3,29 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     public float Health;
-    // add some gui stuff here
+    private float _maxHealth;
+
     public GameObject DeathParticle;
     public AudioClip DeathSound;
     private AudioSource _audioSource;
 
+    [SerializeField]
+    private Gradient _asteroidColor;
+    private Material _asteroidMat;
     private void Start()
     {
         _audioSource = gameObject.AddComponent<AudioSource>();
+        _asteroidMat = GetComponent<Renderer>().material;
+        _maxHealth = Health;
+
+        _asteroidMat.SetColor("_Color", _asteroidColor.Evaluate(1));
     }
 
     public void DoDamage(float damage) 
     {
         Health -= damage;
-        // update gui
+        _asteroidMat.SetColor("_Color", _asteroidColor.Evaluate(Health / _maxHealth));
+
         if (Health <= 0) 
         {
             Instantiate(DeathParticle, transform.position, Quaternion.identity);
